12995ab42dfd112437fc3b9bfe02d83d656a7e95
Erik Stainsby <erik.stainsby@roaringsky.ca>
2012-03-11 14:58:01 -0700

    RSCommentRule is a class; RSIntermediateRuleDelegate handles parts table, now owns -composeRule which preps parts into objects for passing to the primary rule table

---
+++ RSTrixie/RSTrixie/RSIntermediateRuleDelegate.h
@@ -0,0 +1,13 @@
+//
+//  RSIntermediateRuleDelegate.h
+//  RSTrixie
+//
+//  Created by Erik Stainsby on 12-03-10.
+//  Copyright (c) 2012 Roaring Sky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RSIntermediateRuleDelegate : NSObject
+
+@end
---
+++ RSTrixie/RSTrixie/RSIntermediateRuleDelegate.m
@@ -0,0 +1,13 @@
+//
+//  RSIntermediateRuleDelegate.m
+//  RSTrixie
+//
+//  Created by Erik Stainsby on 12-03-10.
+//  Copyright (c) 2012 Roaring Sky. All rights reserved.
+//
+
+#import "RSIntermediateRuleDelegate.h"
+
+@implementation RSIntermediateRuleDelegate
+
+@end
